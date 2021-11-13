f4482ae0e429e4a09fa91540ff57fe7f2e7b9976
SefiShalom <sefi@moonshot.co.il>
2021-11-13 23:20:03 +0200

    added env port

--- src/server.ts
+++ src/server.ts
@@ -7,9 +7,10 @@ import { CoinsController } from './controllers/coinsController';
 import { TYPES } from './types';
 
 const startServer = async () => {
+    const port = process.env.PORT || 3000 as any;
     const app = new App([
         container.get<CoinsController>(TYPES.CoinsController)
-    ], 3000);
+    ], port);
     app.listen();
 }
 

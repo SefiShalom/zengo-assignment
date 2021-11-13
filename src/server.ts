eae4dbabc58024bc720bf048329a59e80263ae67
SefiShalom <sefi@moonshot.co.il>
2021-11-13 23:37:15 +0200

    fixed port initialization

--- src/server.ts
+++ src/server.ts
@@ -7,10 +7,10 @@ import { CoinsController } from './controllers/coinsController';
 import { TYPES } from './types';
 
 const startServer = async () => {
-    const port = process.env.PORT || 3000 as any;
-    const app = new App([
+    const port = process.env.PORT || 3000;
+        const app = new App([
         container.get<CoinsController>(TYPES.CoinsController)
-    ], port);
+    ], port as any);
     app.listen();
 }
 
